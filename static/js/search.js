45b90ec5c55c8c2c419b89441f2729b840fa132a
minisbett <39670899+minisbett@users.noreply.github.com>
2024-02-06 18:45:20 +0100

    Fix searchbar

--- static/js/search.js
+++ static/js/search.js
@@ -3,64 +3,69 @@ const inputElement = document.getElementById('search-txt');
 function handleFocus() {
   $('#search-txt').css('min-width', '350px');
   $('#search-results').fadeIn();
-  $('#search-results').css('visibility', 'visible');
 }
 
 function handleBlur() {
-  if (inputElement.value !== "") {
+  if ($('#search-results').contains(document.activeElement()))
     return;
-  } else {
-    $('#search-txt').css('min-width', '0vw');
-    $('#search-results').fadeOut();
-  }
+  
+  $('#search-txt').css('min-width', '0vw');
+  $('#search-results').fadeOut();
 }
 
 function search() {
   const searchtxt = document.getElementById("search-txt").value;
   const domain = 'kokisu.moe';
-  if (searchtxt === "" || searchtxt === " ") {
+  const parentElement = document.getElementById('search-results');
+  if (searchtxt === "") {
+    parentElement.innerHTML = '';
+    $('#search-results').css('visibility', 'hidden');
     return;
-  } else {
-    $.ajax({
-      url: `${window.location.protocol}//api.${domain}/v1/search_players?q=${searchtxt}`,
-      type: 'GET',
-      success: function (data) {
-        const parentElement = document.getElementById('search-results');
-        parentElement.innerHTML = '';
-        for (let i = 0; i < data.results; i++) {
-          const wrapper = document.createElement('div');
-          wrapper.setAttribute('class', 'result-wrapper');
-          wrapper.setAttribute('id', `result${i}`);
-          wrapper.addEventListener('click', function() {
-            location.href = `https://${domain}/u/${data.result[i].id}`;
-          });
-          const image = document.createElement('div');
-          image.setAttribute('id', 'result-image');
-          image.setAttribute('style', `background-image: url(https://a.${domain}/${data.result[i].id});`);
-          const name = document.createElement('p');
-          name.setAttribute('id', 'result-name');
-          name.innerHTML = data.result[i].name;
-          parentElement.appendChild(wrapper);
-          wrapper.appendChild(image);
-          wrapper.appendChild(name);
-        }
-      },
-      error: function() {
-        const parentElement = document.getElementById('search-results');
-        parentElement.innerHTML = '';
-        const name = document.createElement('div');
-        name.setAttribute('id', 'nothing');
-        name.innerText = 'Nothing Found!';
-        parentElement.appendChild(name);
-      }
-    });
   }
+
+  $.ajax({
+    url: `${window.location.protocol}//api.${domain}/v1/search_players?q=${searchtxt}`,
+    type: 'GET',
+    success: function (data) {
+      parentElement.innerHTML = '';
+      if (document.getElementById("search-txt").value === "") {
+        $('#search-results').css('visibility', 'hidden');
+        return;
+      }
+
+      $('#search-results').css('visibility', 'visible');
+      for (let i = 0; i < data.results; i++) {
+        const wrapper = document.createElement('div');
+        wrapper.setAttribute('class', 'result-wrapper');
+        wrapper.setAttribute('id', `result${i}`);
+        wrapper.addEventListener('click', function () {
+          location.href = `https://${domain}/u/${data.result[i].id}`;
+        });
+        const image = document.createElement('div');
+        image.setAttribute('id', 'result-image');
+        image.setAttribute('style', `background-image: url(https://a.${domain}/${data.result[i].id});`);
+        const name = document.createElement('p');
+        name.setAttribute('id', 'result-name');
+        name.innerHTML = data.result[i].name;
+        parentElement.appendChild(wrapper);
+        wrapper.appendChild(image);
+        wrapper.appendChild(name);
+      }
+    },
+    error: function () {
+      const parentElement = document.getElementById('search-results');
+      parentElement.innerHTML = '';
+      const name = document.createElement('div');
+      name.setAttribute('id', 'nothing');
+      name.innerText = 'Nothing Found!';
+      parentElement.appendChild(name);
+    }
+  });
 }
 
 inputElement.addEventListener('focus', handleFocus);
 inputElement.addEventListener('blur', handleBlur);
-inputElement.addEventListener('change', search);
 
-$("#search-txt").on('keyup keydown', function (e) {
+$("#search-txt").on('input', function (e) {
   search();
 });
